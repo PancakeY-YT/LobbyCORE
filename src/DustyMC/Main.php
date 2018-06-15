6e49c107d4335646d7769b491aa066c7c28bb2f1
EinfachNurNils <39995171+ByxNilsGFXx@users.noreply.github.com>
2018-06-15 18:20:44 +0200

    Create Main.php

---
+++ src/DustyMC/Main.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace DustyMC;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\item\Item;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use muqsit\invmenu\InvMenuHandler;
+use pocketmine\Server;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener {
+
+	public function onEnable() {
+    		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+    	}
+    
+    	public function onDisable() {
+    		foreach($this->getServer()->getOnlinePlayers() as $p) {
+			$p->transfer("82.211.44.7", "19132");
+		}
+   	}
+    
+    	public function onJoin(PlayerJoinEvent $event) {
+    	
+        	$player = $event->getPlayer();
+        	$name = $player->getName();
+        	$this->Main($player);
+        	$event->setJoinMessage("§7[§a+§7] §a" . $name);
+        	
+    	}
+    	
+    	public function onQuit(PlayerQuitEvent $event) {
+    	
+        	$player = $event->getPlayer();
+        	$name = $player->getName();
+        	$event->setQuitMessage("§7[§4-§7] §4" . $name);
+        	
+    	}
+    	
+    	public function Main(Player $player) {
+        	$player->getInventory()->clearAll();
+        	$player->getInventory()->setItem(4, Item::get(345)->setCustomName(TextFormat::YELLOW."Navigator"));
+        	$player->getInventory()->setItem(8, Item::get(399)->setCustomName(TextFormat::GREEN."Lobbys"));
+            $player->getInventory()->setItem(0, Item::get(397 , 3)->setCustomName(TextFormat::AQUA."Dein Profil"));
+
+       }
+    	
+    	public function onInteract(PlayerInteractEvent $event) {
+       		$player = $event->getPlayer();
+        	$item = $player->getInventory()->getItemInHand();
+        
+        	if($item->getCustomName() == TextFormat::YELLOW . "Navigator") {
+            		$menu = new Navigator($this);
+             		$menu->Navigator($player);  
+   
+             	}
+
+            if($item->getCustomName() == TextFormat::GREEN . "Lobbys") {
+            		$menu = new Lobbys($this);
+             		$menu->Lobbys($player);
+             
+                 }
+                 
+           if($item->getCustomName() == TextFormat::AQUA . "Dein Profil") {
+                  $menu = new Profil($this);
+             	    $menu->Profil($player);
+             	    
+	 }
+  }
+}
